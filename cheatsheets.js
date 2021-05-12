5578548a85ee3524ac3196ebfe7fb402564e0b41
Lars Kort <2412670+LTKort@users.noreply.github.com>
2021-05-12 14:26:08 +0200

    Update cheatsheets.js

--- cheatsheets.js
+++ cheatsheets.js
@@ -18,6 +18,7 @@ module.exports = [
             "common-commands",
             "forms",
             "http-client",
+            "routes",
             "mails",
             "sessions",
         ]),
